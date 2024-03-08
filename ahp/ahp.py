acbd370769983eac2435e01b8511e7270a7fa8f4
Febri Nur Rohman <nurrohman.febri@gmail.com>
2024-03-09 05:56:55 +0700

    Refactoring, largest eigen value bug fixing, remove numpy module

--- ahp/ahp.py
+++ ahp/ahp.py
@@ -1,63 +1,102 @@
-import numpy as np
+def sum_comparison_matrix_rows(number_of_criteria):
+    sum_matrix_columns = []
+
+    for _ in range(number_of_criteria):
+        sum_matrix_columns.append(0)
+
+    for row in comparison_matrix:
+        for index, item in enumerate(row):
+            sum_matrix_columns[index] = sum_matrix_columns[index] + item
+
+    return sum_matrix_columns
 
 
-def get_number_of_criteria(comparison_matrix):
-    matrix_shape = comparison_matrix.shape
-    count_matrix_rows = matrix_shape[0]
+def build_normalized_matrix(number_of_criteria, sum_matrix_columns):
+    normalized_matrix = []
 
-    return count_matrix_rows
+    for row_number in range(number_of_criteria):
+        normalized_row = []
 
-
-def get_normalized_matrix(comparison_matrix):
-    sum_matrix_columns = np.sum(comparison_matrix, axis=0)
-    normalized_matrix = comparison_matrix / sum_matrix_columns
-
+        for column_number in range(number_of_criteria):
+            normalized_row.append(comparison_matrix[row_number][column_number] / sum_matrix_columns[column_number])
+        
+        normalized_matrix.append(normalized_row)
+    
     return normalized_matrix
 
 
-def calculate_priority_vector(normalized_matrix):
-    average_normalized_matrix_rows = np.mean(normalized_matrix, axis=1)
+def calculate_priority_vector(normalized_matrix, number_of_criteria):
+    priority_vector = []
 
-    return average_normalized_matrix_rows
+    for row in normalized_matrix:
+        priority_vector.append(sum(row) / number_of_criteria)
 
-
-def calculate_consistency_index(priority_vector, comparison_matrix, number_of_criteria):
-    w = priority_vector
-    max_eigenvalue = np.dot(w, np.dot(comparison_matrix, w))
-    consistency_index = (max_eigenvalue - number_of_criteria) / (number_of_criteria - 1)
-
-    return consistency_index
+    return priority_vector
 
 
-def calculate_consistency_ratio(number_of_criteria, consistency_index):
-    # (Table of random index values can be found in AHP literature)
-    random_index = {1: 0.00, 2: 0.00, 3: 0.58, 4: 0.89, 5: 1.12, 6: 1.24, 7: 1.35, 8: 1.47, 9: 1.59}
+def calculate_largest_eigen_value(number_of_criteria, priority_vector, sum_matrix_columns):
+    multiplied = []
 
-    if number_of_criteria not in random_index:
-        print("WARNING: Random index (RI) not available for matrix size", number_of_criteria)
-        consistency_ratio = np.nan  # Not a number
-    else:
-        consistency_ratio = consistency_index / random_index[number_of_criteria]
+    for i in range(number_of_criteria):
+        multiplied.append(priority_vector[i] * sum_matrix_columns[i])
+    
+    return sum(multiplied)
 
-    return consistency_ratio
+
+def calculate_consistency_index(largest_eigen_value, number_of_criteria):
+    return (largest_eigen_value - number_of_criteria) / (number_of_criteria - 1)
+
+
+def calculate_consistency_ratio(consistency_index, number_of_criteria):
+    random_index = {1: 0.00, 2: 0.00, 3: 0.58, 4: 0.90, 5: 1.12, 6: 1.24, 7: 1.32, 8: 1.41, 9: 1.45}
+    
+    return consistency_index / random_index[number_of_criteria]
 
 
 def ahp(comparison_matrix):
-    number_of_criteria = get_number_of_criteria(comparison_matrix)
-    normalized_matrix = get_normalized_matrix(comparison_matrix)
-    priority_vector = calculate_priority_vector(normalized_matrix)
-    consistency_index = calculate_consistency_index(priority_vector, comparison_matrix, number_of_criteria)
-    consistency_ratio = calculate_consistency_ratio(number_of_criteria, consistency_index)
+    number_of_criteria = len(comparison_matrix)
+    sum_matrix_columns = sum_comparison_matrix_rows(number_of_criteria)
+
+    # print(sum_matrix_columns)
+    # print("\n")
+
+
+    normalized_matrix = build_normalized_matrix(number_of_criteria, sum_matrix_columns)
+
+    # print(normalized_matrix)
+    # print("\n")
+
+
+    priority_vector = calculate_priority_vector(normalized_matrix, number_of_criteria)
+    
+    # print(priority_vector)
+    # print("\n")
+
+
+    largest_eigen_value = calculate_largest_eigen_value(number_of_criteria, priority_vector, sum_matrix_columns)
+    
+    # print(largest_eigen_value)
+    # print("\n")
+    
+    
+    consistency_index = calculate_consistency_index(largest_eigen_value, number_of_criteria)
+    # print("CI:", consistency_index)
+    # print("\n")
+
+
+    consistency_ratio = calculate_consistency_ratio(consistency_index, number_of_criteria)
+    # print("CR:", consistency_ratio)
+    # print("\n")
 
     return priority_vector, consistency_ratio
 
 
-# Example usage with error handling
-comparison_matrix = np.array(
-    [[1, 3, 5],
-    [1/3, 1, 4],
-    [1/5, 1/4, 1]]
-)
+comparison_matrix = [
+    [1, 3, 7, 9],
+    [1/3, 1, 5, 7],
+    [1/7, 1/5, 1, 3],
+    [1/9, 1/7, 1/3, 1]
+]
 
 priority_vector, consistency_ratio = ahp(comparison_matrix)
 
